fe86259cf9865a9917069bd1699da414b8746fdd
aakansha-gkmit <aakansha@gkmit.co>
2025-12-07 18:39:55 +0530

    feat: add fake student data

---
+++ generate_students.py
@@ -0,0 +1,25 @@
+import csv
+from faker import Faker
+import random
+
+fake = Faker()
+
+def generate_students_csv(filename, count=50):
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        
+        # CSV Header
+        writer.writerow(["roll_no", "name", "phone_no", "email"])
+        
+        for i in range(count):
+            name = fake.name()
+            roll_no = f"CS-{100+i}"
+            phone_no = fake.numerify(text="##########")  # 10 digits
+            email = fake.email()
+            
+            writer.writerow([roll_no, name, phone_no, email])
+    
+    print(f"Generated {count} student records into {filename}")
+
+if __name__ == "__main__":
+    generate_students_csv("students.csv", count=10)
